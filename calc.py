43ac4ff37d8a392e31aec4558106619d3d0ed749
duncanrules <wrule1@babson.edu>
2016-09-01 15:26:35 -0400

    calc excersizes in assignment 1

--- calc.py
+++ calc.py
@@ -1,1 +1,7 @@
-print('Q1:',42*60+42)
+seconds = 42*60+42      #convert to seconds
+miles = 10/1.61         #convert to miles
+spm = seconds/miles     #seconds per mile
+apm = spm/60            #average pace in minutes
+print('Q1:',seconds)
+print('Q2:',miles)
+print('Q3: average pace =',apm,' mph =', 60/apm)
