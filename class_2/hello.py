ab70a2231c497956fd803010f1da1937d0d9ea09
duncanrules <wrule1@babson.edu>
2016-09-06 14:49:54 -0400

    class 2

--- class_2/hello.py
+++ class_2/hello.py
@@ -1,5 +1,6 @@
 print('Please enter your name')
 name = input()
 print('Hello,', name)
-
 print('Congratulations %s, you won the %dth Academy Award!!!!!' % (name, 88))
+
+print('Growth rate: %d%%' % 7)
