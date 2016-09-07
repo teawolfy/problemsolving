f483fa3e8c0b156d98a2111ec62787654a7ef99e
duncanrules <wrule1@babson.edu>
2016-09-06 22:40:55 -0400

    class 2 excersize

---
+++ class_2/calc.py
@@ -0,0 +1,16 @@
+pi = 3.14159265359                
+vol = (4/3)*pi*(5**3)
+print('Question 1:', vol)
+
+book_price = 24.95*.6
+shipping_price = 3+.75*59
+print('Question 2:', book_price + shipping_price)
+
+easy_time = 2*8.25
+tempo_time = 3*7.2
+total = easy_time + tempo_time
+end_minutes = total - 8             #the amount of minutes past 7:00
+print('Question 3: 7:%d' % (end_minutes))
+
+percent = ((89-82)/82)*100
+print('Question 4: %.1f%%' % percent)
