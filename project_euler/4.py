cb1ca066be32fc25ccd9294590b1134db8542c66
duncanrules <wrule1@babson.edu>
2016-09-08 16:21:12 -0400

    Question 4

--- project_euler/4.py
+++ project_euler/4.py
@@ -1,1 +1,13 @@
+def is_palindrome(num):
+    if str(num) == (str(num)[::-1]):
+        return True
+    else:
+        return False
 
+pal = 0
+for x in range(100, 999):
+    for y in range(x + 1, 999):
+        product = x * y
+        if product > pal and is_palindrome(product) == True:
+            pal = product
+print(pal)
