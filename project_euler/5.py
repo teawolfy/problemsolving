183c4f2fc325de7d3e437bd18facaa1cefc42ce4
duncanrules <wrule1@babson.edu>
2016-09-10 19:29:15 -0400

    Project Euler Problem 5

--- project_euler/5.py
+++ project_euler/5.py
@@ -1,1 +1,10 @@
-
+smalls = 0
+count = 0
+while smalls == 0:
+    count += 1
+    for i in range(2, 20):
+        if count % i != 0:
+            break
+    else:
+        smalls = count
+print(smalls)
