54475686acc1165323e8c88efd97b77ef5bce24b
duncanrules <wrule1@babson.edu>
2016-09-18 20:35:32 -0400

    Working on class 5 hw

--- class_5/classwork.py
+++ class_5/classwork.py
@@ -1,18 +1,20 @@
 import turtle
 import math
 jerry = turtle.Turtle()
+jerry.pensize(4)
+jerry.hideturtle()
+
 
 print(jerry)
 
-def square(t, length):
-    for i in range(0,4):
+def polyline(t, n, length, angle):
+    for i in range(n):
         t.fd(length)
-        t.lt(90)
+        t.lt(angle)
 
 def polygon(t, n, length):
-    for i in range(0,n):
-        t.fd(length)
-        t.lt(360/n)
+    angle = 360.0 / n
+    polyline(t, n, length, angle)
 
 def circle(t, r):
     circumference = 2 * math.pi * r
@@ -24,21 +26,20 @@ def arc(t, r, angle):
     arc_length = 2 * math.pi * r * angle / 360
     n = int(arc_length / 3) + 1
     step_length = arc_length / n
-    step_angle = angle / n
-    
-    for i in range(n):
-        t.fd(step_length)
-        t.lt(step_angle)
+    step_angle = float(angle) / n
+    polyline(t, n, step_length, step_angle)
 
-for i in range(0,10):
-    arc(jerry,100,90)
-    jerry.lt(90)
-jerry.lt(90)
-for i in range(0,10):
-    arc(jerry,100,90)
-    jerry.lt(90)
+def petal(t, r, angle):
+    for i in range(0,2):
+        arc(t, r, angle)
+        t.lt(180-angle)
 
-
+for i in range(0,6):
+    petal(jerry, 200, 360/6)
+    jerry.lt(360/6)
+arc(jerry, 200, 360/6)
+jerry.lt(61)
+circle(jerry,200)
 
 
 
