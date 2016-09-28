f6223201679d90ef91e4c243b05aa67e7945f858
duncanrules <wrule1@babson.edu>
2016-09-28 16:15:07 -0400

    Work on Assignment 1

--- assignment_1/factoring.py
+++ assignment_1/factoring.py
@@ -1,1 +1,14 @@
+from math import sqrt
 
+i = float(input('Please input an integer to be factored '))
+
+def my_factor(x):
+    factor_list = []
+    x = int(x)
+    for num in range(1, int(sqrt(x)+1)):
+        if x % num == 0:
+            factor_list.append(num)
+            factor_list.append(int(x / num))
+    return factor_list
+
+print(my_factor(i))
