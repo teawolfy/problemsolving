3989f8544350d5479b8b7147fbf3808cc1a7bb72
duncanrules <wrule1@babson.edu>
2016-10-13 17:09:39 -0400

    Class_12

--- class_11/dictionaries.py
+++ class_11/dictionaries.py
@@ -1,11 +1,1 @@
 #exercise 1
-def getavailableletters(lettersguessed):
-    import string
-    possibleletters = list(string.ascii_lowercase)
-    for letters in lettersguessed:
-        if letters in possibleletters:
-            possibleletters.remove(letters)
-    availableletters = ''.join(possibleletters)
-    return availableletters
-
-print(getavailableletters(['a','c']))
